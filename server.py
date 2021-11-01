25216388d2e4cbdd9a78732488e05f172212d7c0
Roscoe <elysiaroscoe@gmail.com>
2021-10-31 22:19:08 -0700

    mid commit

--- server.py
+++ server.py
@@ -1,24 +1,21 @@
 
 from flask import Flask, render_template, request, redirect, session
+import random
+
 app = Flask(__name__)
 app.secret_key = "lets guess a number"
 
 @app.route('/')
 def index():
-    if 'guess' in session:
-        print('yes')
-    else:
-        print('no')
+    if 'num' not in session:
+        session['num'] = random.randint(1,101)
+        print(session['num'])
     return render_template("index.html")
 
-# @app.route('/calculate')
-# def isitequal():
-    # if session['guess'] < session['random']:
-
-    # elif session['guess'] > session['random']:
-        
-    # else
-
+@app.route('/guess', methods=['POST'])
+def guess():
+    session['guess'] = int(request.form['guess'])
+    print(session['guess'])
     redirect('/')
 
 @app.route('/destroy_session')
